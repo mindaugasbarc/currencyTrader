0b15550b5c04a7d2171432710f9bc1d9dc6eee4c
Mindaugas Barcauskas <Mindaugas.Barcauskas1@T-Mobile.com>
2019-07-25 23:10:01 -0700

    adding user sign-up functionality

--- src/main/java/com/learning/spring/user/model/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.learning.spring.user.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
