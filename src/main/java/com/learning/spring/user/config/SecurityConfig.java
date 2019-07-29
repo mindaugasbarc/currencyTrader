4d9b8e46ec99a623d3a7a4d47ce29e4c8bde69cd
Mindaugas Barcauskas <Mindaugas.Barcauskas1@T-Mobile.com>
2019-07-28 23:19:00 -0700

    working sign-up endpoint

--- src/main/java/com/learning/spring/user/config/SecurityConfig.java
+++ src/main/java/com/learning/spring/user/config/SecurityConfig.java
@@ -2,13 +2,31 @@ package com.learning.spring.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
