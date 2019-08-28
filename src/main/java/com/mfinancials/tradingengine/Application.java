346b25ea0ad39e8c3395bb40f7e8a32e46861c88
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2019-08-28 14:49:47 +0300

    cleaning the application class

--- src/main/java/com/mfinancials/tradingengine/Application.java
+++ src/main/java/com/mfinancials/tradingengine/Application.java
@@ -1,51 +1,14 @@
 package com.mfinancials.tradingengine;
 
-import com.mfinancials.tradingengine.money.domain.model.Balance;
-import com.mfinancials.tradingengine.money.domain.model.Currency;
-import com.mfinancials.tradingengine.money.domain.model.Money;
-import com.mfinancials.tradingengine.money.domain.repository.CurrencyRepository;
-import com.mfinancials.tradingengine.user.domain.UserRepository;
-import com.mfinancials.tradingengine.user.domain.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
-    @Autowired
-    private CurrencyRepository currencyRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-    @Override
-    public void run(String... args) throws Exception {
-	    Currency usd = new Currency("USD", BigDecimal.valueOf(1));
-	    Currency eur = new Currency("EUR", BigDecimal.valueOf(1.5));
-
-        currencyRepository.save(usd);
-        currencyRepository.save(eur);
-        currencyRepository.save(new Currency("CAD", BigDecimal.valueOf(0.8)));
-
-        Balance balance = new Balance();
-        balance.addMoney(new Money(usd, 20));
-        balance.addMoney(new Money(eur, 20));
-
-        Balance balance2 = new Balance();
-        balance2.addMoney(new Money(usd, 100));
-        balance2.addMoney(new Money(eur, 100));
-        userRepository.save(new User(
-                "minde", balance));
-        userRepository.save(new User("jonas", balance2));
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
+
+
 }
